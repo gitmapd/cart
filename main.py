f4b84fad2e11e946d27f4220ec3bc8d1c873403a
m <m@mPC.lan>
2025-04-13 15:00:25 +0100

    Code

--- main.py
+++ main.py
@@ -40,8 +40,21 @@ def main():
     item = Item()
     item.set_item({"item_id": 101, "name": "butter"},)
     items.items.append(item)
-    for i in range(len(items.items)):
-        print(item.item['name'])
+    
+    prod_table = table.Table(show_header=True, header_style="bold magenta", expand=True, highlight=True)
+    prod_table.add_column("Código", justify="center")
+    prod_table.add_column("Nome", justify="center")
+    for i in track(range(len(items.items)), description="A Processar..."):
+            its = items.items[i].item
+            print(its['name'])
+            
+            #it = '  '.join(str(x['item_id']).ljust(3) for x in bet)
+            #prod_table.add_row('Aposta '+str(i + 1),it)
+
+    console.rule("Boletim", style="bold yellow")
+    console.print(prod_table)
+    #for i in range():
+    #    print(item.item['name'])
         
     
     print()
