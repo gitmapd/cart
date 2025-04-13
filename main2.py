b90788e9b11e80be349d6bd2233893d5a40979c4
m <m@mPC.lan>
2025-04-13 20:30:37 +0100

    Code

--- main2.py
+++ main2.py
@@ -9,28 +9,28 @@ from loader import load_items_from_file
 
 console = Console()
 class Item:
-    def __init__(self, codigo, nome, preco):
-        self.codigo = codigo
-        self.nome = nome
-        self.preco = preco
+    def __init__(self, code, name, price):
+        self.code = code
+        self.name = name
+        self.price = price
     
     def __repr__(self):
         return (
-            f"({self.codigo}) {self.nome} - €{self.preco:.2f}"
+            f"({self.code}) {self.name} - €{self.preco:.2f}"
         )
 
 class Inventory:
     def __init__(self):
         self.items = {}  
 
-    def add_item(self, codigo, nome, preco):
-        self.items[codigo] = Item(codigo, nome, preco)
-    def get_item(self, codigo):
-        return self.items.get(codigo)
+    def add_item(self, code, name, preco):
+        self.items[code] = Item(code, name, preco)
+    def get_item(self, code):
+        return self.items.get(code)
 
 
     def __repr__(self):
-        return f"{self.nome} - {self.descricao}"
+        return f"{self.name} - {self.description}"
 
    
 
@@ -41,10 +41,10 @@ def main():
     
     prod_table = table.Table(show_header=True, header_style="bold magenta", expand=True, highlight=True)
     prod_table.add_column("Código", justify="center")
-    prod_table.add_column("Nome", justify="center")
+    prod_table.add_column("name", justify="center")
     for k,v in track(inventory.items.items(), description="A Processar..."):
             print(v)
-            #prod_table.add_row('Nome',it)
+            #prod_table.add_row('name',it)
 
     console.rule("Boletim", style="bold yellow")
     console.print(prod_table)
