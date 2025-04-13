6da7383add394df78c746689bd8d431504eb4161
m <m@mPC.lan>
2025-04-13 20:18:49 +0100

    Code

--- main2.py
+++ main2.py
@@ -1,6 +1,13 @@
 from collections import defaultdict
 from datetime import datetime
+import rich
+from rich import table, prompt
+import datetime
+from rich.progress import track
+from rich.console import Console
 from loader import load_items_from_file
+
+console = Console()
 class Item:
     def __init__(self, codigo, nome, preco):
         self.codigo = codigo
@@ -27,10 +34,22 @@ class Inventory:
 
    
 
-inventory  = Inventory()
 
-load_items_from_file("frutas.csv",inventory)
+def main():
+    inventory  = Inventory()
+    load_items_from_file("frutas.csv",inventory)
+    
+    prod_table = table.Table(show_header=True, header_style="bold magenta", expand=True, highlight=True)
+    prod_table.add_column("Código", justify="center")
+    prod_table.add_column("Nome", justify="center")
+    for k,v in track(inventory.items.items(), description="A Processar..."):
+            print(v)
+            #prod_table.add_row('Nome',it)
 
-apple =inventory.get_item("F2")
-print(apple)
+    console.rule("Boletim", style="bold yellow")
+    console.print(prod_table)
+    apple =inventory.get_item("F2")
+    print(apple)
 
+if __name__ == '__main__':
+        main()
